645ae8e1c056e6d7783b1672e7ba5247ce83f0e0
delisnt <danieledelise12@gmail.com>
2023-10-26 13:26:13 +0200

    added a couple features that let you color the grid with random colors, an eraser and made so that for drawing in general you need to press a button

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let button = document.querySelector("#pick-size")
+let eraserButton = document.querySelector('#eraser');
+let blackButton = document.querySelector('#black');
+let rgbButton = document.querySelector('#rainbow');
+let sizeButton = document.querySelector("#pick-size");
 let container = document.querySelector('#container');
-const btn = document.querySelector('#cleargrid');
-button.addEventListener("click", pickGridSize);
+const clearGridButton = document.querySelector('#cleargrid');
+let hasGridSizeRun = false;
+sizeButton.addEventListener("click", pickGridSize);
 function genDivs(rows, columns) {
+    hasGridSizeRun = true;
     for (let i = 0; i < rows * columns; i++){
         const cell = document.createElement('div');
         cell.style.cssText = 'border: 1px solid black; box-sizing: border-box;';
@@ -16,20 +21,38 @@ function genDivs(rows, columns) {
     cells.forEach(cell => {
         cell.style.flex = `0 0 ${cellWidth}`;
         cell.style.height = cellHeight;
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = 'black';
+        blackButton.addEventListener('click', () => {
+            cell.addEventListener('mouseover', () =>{
+                cell.style.backgroundColor = 'black'
+            })
         })
-        btn.addEventListener('click', () =>{
+        clearGridButton.addEventListener('click', () =>{
             cell.style.backgroundColor = 'white';
         })
-
+        rgbButton.addEventListener('click', () => {
+            cell.addEventListener('mouseover', () =>{
+                cell.style.backgroundColor = pickRandomRGBValue();
+            })
+        })
+        eraserButton.addEventListener('click', () =>{
+            cell.addEventListener('mouseover', () =>{
+                cell.style.backgroundColor = 'white';
+            })
+        })
     });
 }
 
     function pickGridSize() {
-        const gridValue = prompt("Pick a size for your grid!")
-        genDivs(gridValue,gridValue)
+        const gridValue = prompt("Pick a size for your grid!"); 
+        genDivs(gridValue,gridValue);
     }
 
+    function pickRandomRGBValue() {
+        let x = Math.floor(Math.random()* 256);
+        let y = Math.floor(Math.random()* 256);
+        let z = Math.floor(Math.random()* 256);
+        let RGBValue = "rgb(" + x + "," + y + "," + z +")"
+       return RGBValue;
+    }
 
-
+    pickRandomRGBValue()
